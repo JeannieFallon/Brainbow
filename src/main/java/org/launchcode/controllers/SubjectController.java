ed8465d044f748596077cca797c23d99e070cf32
Jeannie Fallon <jeanniefallon@gmail.com>
2017-05-22 22:18:25 -0400

    Add and display Subjects

--- src/main/java/org/launchcode/controllers/SubjectController.java
+++ src/main/java/org/launchcode/controllers/SubjectController.java
@@ -2,10 +2,13 @@ package org.launchcode.controllers;
 
 import org.launchcode.data.SubjectDao;
 import org.launchcode.models.Subject;
+import org.launchcode.models.SubjectColor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by jeannie on 5/1/17.
@@ -20,14 +23,24 @@ public class SubjectController {
 
     @RequestMapping(value = "")
     public String index(Model model) {
+        Iterable<Subject> subjects = subjectDao.findAll();
         model.addAttribute("title", "Current Subjects");
+        model.addAttribute("subjects",subjects);
         return "brainbow/subject/index";
     }
 
-    @RequestMapping(value = "add")
+    @RequestMapping(value = "add", method = RequestMethod.GET)
     public String add(Model model) {
         model.addAttribute("title", "Add Subject");
+        model.addAttribute(new Subject());
+        model.addAttribute("subjectColors", SubjectColor.values());
         return "brainbow/subject/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String add(@ModelAttribute Subject subject, Model model) {
+        subjectDao.save(subject);
+        return "redirect:";
     }
 
     @RequestMapping(value = "remove")
