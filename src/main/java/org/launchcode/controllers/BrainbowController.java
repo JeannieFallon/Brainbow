2f9ef3b08321d7d71cc0da6239a21bbc92c950cf
Jeannie Fallon <jeanniefallon@gmail.com>
2017-05-27 11:14:34 -0400

    Begin to implement 'Log Time'

--- src/main/java/org/launchcode/controllers/BrainbowController.java
+++ src/main/java/org/launchcode/controllers/BrainbowController.java
@@ -1,10 +1,12 @@
 package org.launchcode.controllers;
 
+import org.launchcode.data.BrainbowDao;
 import org.launchcode.data.SubjectDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by jeannie on 4/29/17.
@@ -14,12 +16,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class BrainbowController {
 
     @Autowired
+    private BrainbowDao brainbowDao;
+
+    @Autowired
     private SubjectDao subjectDao;
 
-    @RequestMapping(value = "")
+    @RequestMapping(value = "", method = RequestMethod.GET)
     public String index(Model model) {
         model.addAttribute("title", "Home");
+        //pass in list of subjects to display on eventual Brainbow graphic
+        //and to display dropdown of subjects to edit
         model.addAttribute("subjects",subjectDao.findAll());
+
         return "brainbow/index";
     }
 
