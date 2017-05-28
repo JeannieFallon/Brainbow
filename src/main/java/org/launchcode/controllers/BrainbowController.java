01f20f4ab385e39f4b87f13efce3cf0ed63f0787
Jeannie Fallon <jeanniefallon@gmail.com>
2017-05-28 19:11:05 -0400

    Implement suggest next subject to user
    Find the lowest worktime of all subjects. Suggest to user that (s)he should devote time to subject(s) with the lowest worktime.

--- src/main/java/org/launchcode/controllers/BrainbowController.java
+++ src/main/java/org/launchcode/controllers/BrainbowController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+
 /**
  * Created by jeannie on 4/29/17.
  */
@@ -19,13 +21,51 @@ public class BrainbowController {
     @Autowired
     private SubjectDao subjectDao;
 
-
     @RequestMapping(value = "")
     public String index(Model model) {
+
+        Iterable<Subject> subjects = subjectDao.findAll();
+
+        /* Calculate which subject(s) user should work on next, based on lowest worktime */
+
+        //declare list of subjects with lowest worktime
+        //use ArrayList in case multiple subjects share same lowest worktime
+        ArrayList<Subject> lowestTimeSubjects = new ArrayList<Subject>();
+
+        //check for no subjects
+        if(subjects != null) {
+
+            //convert Iterable to List in order to access first subject directly
+            ArrayList<Subject> subjectsList = new ArrayList<Subject>();
+                for(Subject subject: subjects) {
+                    subjectsList.add(subject);
+                }
+
+            //set lowest time to first subject as baseline
+            int lowestTime = subjectsList.get(0).getTime();
+
+            for(Subject subject : subjectsList) {
+
+                int subjectTime = subject.getTime();
+
+                //for subject with new lowest time, reset baseline and list
+                if(subjectTime < lowestTime) {
+                    lowestTime = subjectTime;
+                    lowestTimeSubjects.clear();
+                    lowestTimeSubjects.add(subject);
+
+                //for subject with equal lowest time, add subject to list
+                } else if(subjectTime == lowestTime) {
+                    lowestTimeSubjects.add(subject);
+                }
+            }
+        }
+
         model.addAttribute("title", "Home");
-        //pass in an iterable of subjects to display in eventual Brainbow graphic
-        model.addAttribute("subjects",subjectDao.findAll());
-        //TODO: add suggestion to work on subject with least time logged
+        //pass in Iterable of subjects to display in eventual Brainbow graphic
+        model.addAttribute("subjects",subjects);
+        //pass in List of subject(s) with lowest worktime to suggest next subject to user
+        model.addAttribute("lowestTimeSubjects",lowestTimeSubjects);
         return "brainbow/index";
     }
 
